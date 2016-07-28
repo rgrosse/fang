db0ddaa02e250398ded0d613f250c9a763d8a67d
Roger Grosse <rgrosse@cs.toronto.edu>
2016-07-28 10:03:20 -0400

    removed config.py (was there by mistake)

--- config.py
+++
@@ -1,8 +0,0 @@
-MNIST_FILE = '/users/rgrosse/research/data/mnist/train-images-idx3-ubyte'
-MNIST_TEST_FILE = '/users/rgrosse/research/data/mnist/t10k-images-idx3-ubyte'
-DATA_DIR = '/users/rgrosse/research/data'
-OUTPUTS_DIR = '/users/rgrosse/research/outputs/fang_debug'
-FIGURES_DIR = '/users/rgrosse/research/figures/fang_debug'
-CODE_DIR = '/users/rgrosse/code/fang'
-
-
