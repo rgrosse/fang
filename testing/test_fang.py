6d63cc4ca6c989dfa805bb25b8ce1f40be31d9da
Roger Grosse <rgrosse@cs.toronto.edu>
2016-07-29 14:19:27 -0400

    removed unused tests

--- testing/test_fang.py
+++ testing/test_fang.py
@@ -2,11 +2,8 @@ import gnumpy as gnp
 import numpy as np
 nax = np.newaxis
 
-import binary_rbms
 import fang
 import fisher
-import generic_rbms
-import rbm_training
 import test_tractable
 import tractable
 from utils import misc
@@ -104,64 +101,4 @@ def check_against_exact():
         Sigma = stats.unary_covariance()
         assert np.max(np.abs(Sigma - G[:NVIS+NHID, :NVIS+NHID])) < 1e-6
                 
-    
-def check_generic_same():
-    params = rbm_training.FANGParams.defaults('pcd')
-    params.start_after = 0    # otherwise the update test is silly
-    WEIGHT_DECAY = 0.001
-    
-    with misc.gnumpy_conversion_check('allow'):
-        bin_rbm = test_tractable.random_rbm(NVIS, NHID)
-        v = gnp.garray(np.random.binomial(1, 0.5, size=(N, NVIS)))
-        h = gnp.garray(np.random.binomial(1, 0.5, size=(N, NHID)))
-        bin_stats = fang.Statistics.from_activations(v, h)
 
-        gen_rbm = generic_rbms.BinaryRBM(generic_rbms.BinaryLayer.Params(bin_rbm.vbias.copy()),
-                                         generic_rbms.BinaryLayer.Params(bin_rbm.hbias.copy()),
-                                         bin_rbm.weights.copy())
-        gen_stats = fang.GenericStatistics.from_activations(generic_rbms.BinaryLayer, v,
-                                                            generic_rbms.BinaryLayer, h)
-
-        assert np.allclose(bin_stats.m_unary, gen_stats.m_unary)
-        assert np.allclose(bin_stats.S_unary, gen_stats.S_unary)
-        assert np.allclose(bin_stats.m_pair, gen_stats.m_pair)
-        assert np.allclose(bin_stats.S_pair, gen_stats.S_pair)
-
-        bin_beta, bin_sigma_sq = bin_stats.compute_regression_weights(params.smoothing)
-        gen_beta, gen_sigma_sq = gen_stats.compute_regression_weights(params.smoothing)
-        assert np.allclose(bin_beta, gen_beta)
-        assert np.allclose(bin_sigma_sq, gen_sigma_sq)
-
-        assert np.allclose(bin_stats.unary_covariance(params.smoothing), gen_stats.unary_covariance(params.smoothing))
-
-        v2 = gnp.garray(np.random.binomial(1, 0.5, size=(N, NVIS)))
-        h2 = gnp.garray(np.random.binomial(1, 0.5, size=(N, NHID)))
-        
-        bin_pos_moments = binary_rbms.Moments.from_activations(v, h)
-        bin_neg_moments = binary_rbms.Moments.from_activations(v2, h2)
-        gen_pos_moments = generic_rbms.Moments(
-            generic_rbms.BinaryLayer.Moments(bin_pos_moments.expect_vis),
-            generic_rbms.BinaryLayer.Moments(bin_pos_moments.expect_hid),
-            bin_pos_moments.expect_prod)
-        gen_neg_moments = generic_rbms.Moments(
-            generic_rbms.BinaryLayer.Moments(bin_neg_moments.expect_vis),
-            generic_rbms.BinaryLayer.Moments(bin_neg_moments.expect_hid),
-            bin_neg_moments.expect_prod)
-
-        lrates = binary_rbms.LearningRates(2., 2., 1.5)
-
-        bin_updater = fang.Updater(params)
-        bin_updater.Lambda = np.linalg.inv(bin_stats.unary_covariance(params.smoothing))
-        bin_updater.beta, bin_updater.sigma_sq = bin_beta, bin_sigma_sq
-        gen_updater = fang.GenericUpdater(params)
-        gen_updater.Lambda = np.linalg.inv(gen_stats.unary_covariance(params.smoothing))
-        gen_updater.beta, gen_updater.sigma_sq = gen_beta, gen_sigma_sq
-
-        bin_updater.apply_update(bin_pos_moments, bin_neg_moments, bin_rbm, WEIGHT_DECAY, lrates)
-        gen_updater.apply_update(gen_pos_moments, gen_neg_moments, gen_rbm, WEIGHT_DECAY, lrates)
-        assert np.allclose(bin_rbm.vbias, gen_rbm.vis.biases)
-        assert np.allclose(bin_rbm.hbias, gen_rbm.hid.biases)
-        assert np.allclose(bin_rbm.weights, gen_rbm.weights)
-        
-
-        
